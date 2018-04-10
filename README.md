07a5d027e7d9eecbe1971b2150364f6b96962fa4
Tim <timwoerner94@web.de>
2018-04-10 11:56:01 +0200

    update README.md, order index.js operations alphabetically

--- README.md
+++ README.md
@@ -1,19 +1,39 @@
 [![Build Status](https://travis-ci.org/sigma3Dev/s3d-fitting-commands.svg?branch=master)](https://travis-ci.org/sigma3Dev/s3d-fitting-commands)
 
 # s3d-fitting-commands
+
 module with all available fitting commands (json) to communicate with the s3DFittingService (c++ websocket service)
 
 ## Installation
 
-  ```npm install s3d-fitting-commands --save```
+`npm install s3d-fitting-commands --save`
 
 ## Testing
 
-  ```npm test```
+`npm test`
 
 ## Overview
 
 ```js
+/**
+ * applyTransformation - generates the json request to apply a transformation
+ *
+ * @param {object} point point to apply transformation to
+ * @param {object} transformation  transformation to apply to the point
+ * @param {number} id an identifier for the generated request
+ * @return {string} the json request representation
+ */
+applyTransformation: function(point, transformation, id)
+
+/**
+ * cardanToQuat - generates the json request to transform cardan rotation to quaternions
+ *
+ * @param {object} coords to be transformed
+ * @param {number} id an identifier for the generated request
+ * @return {string} the json request representation
+ */
+cardanToQuat: function(coords, id)
+
 /**
  * fitCircle3DTscheby - generates the json request to fit a 3D-circle
  * with Tschebyscheff approximation
@@ -25,6 +45,15 @@ module with all available fitting commands (json) to communicate with the s3DFit
 fitCircle3DTscheby: function(points, id)
 
 /**
+ * fitCylinder - generates the json request to fit cylinder
+ *
+ * @param  {Array} points the points used to fit the plane
+ * @param  {number} id an identifier for the generated request
+ * @return {string} the json request representation
+ */
+fitCylinder: function(points, id)
+
+/**
  * fitPlaneL2 - generates the json request to fit a plane
  * with Gauss approximation
  *
@@ -33,6 +62,35 @@ fitCircle3DTscheby: function(points, id)
  * @return {string} the json request representation
  */
 fitPlaneL2: function(points, id)
+
+/**
+ * fitPlaneRansac - generates the json request to fit a plane
+ * with RANSAC
+ *
+ * @param  {Array} points the points used to fit the plane
+ * @param  {number} tolerance the tolerance used to fit the plane
+ * @param  {number} id an identifier for the generated request
+ * @return {string} the json request representation
+ */
+fitPlaneRansac: function(points, tolerance, id)
+
+/**
+ * invertTransformationParameters - generates the json request to invert transformation parameters
+ *
+ * @param {Object} transformation transformation parameters to be inverted
+ * @param {number} id an identifier for the generated request
+ * @return {string} the json request representation
+ */
+invertTransformationParameters: function(transformation, id)
+
+/**
+ * quatToCardan - generates the json request to transform quaternions to cardan rotation
+ *
+ * @param {object} coords to be transformed
+ * @param {number} id an identifier for the generated request
+ * @return {string} the json request representation
+ */
+quatToCardan: function(coords, id)
 
 /**
  * registerPointsInPlane - generates the json request to register
@@ -47,30 +105,11 @@ registerPointsInPlane: function(plane, points, id)
 
 /**
  * transformation3D6W - generates the json request to transform 3D coordinates
- * 
+ *
  * @param  {Array} startPoints the coordinates of the start system
  * @param  {Array} targetPoints the coordinates of the target system
  * @param  {number} id an identifier for the generated request
  * @return {string} the json request representation
  */
 transformation3D6W: function(startPoints, targetPoints, id)
-
-/**
- * invertTransformationParameters - generates the json request to invert transformation parameters
- * 
- * @param {Object} transformation transformation parameters to be inverted
- * @param {number} id an identifier for the generated request
- * @return {string} the json request representation
- */
-invertTransformationParameters: function(transformation, id)
-
-/**
- * applyTransformation - generates the json request to apply a transformation
- * 
- * @param {object} point point to apply transformation to
- * @param {object} transformation  transformation to apply to the point
- * @param {number} id an identifier for the generated request
- * @return {string} the json request representation
- */
-applyTransformation: function(point, transformation, id)
 ```
--- index.js
+++ index.js
@@ -1,5 +1,107 @@
 module.exports = {
   /**
+   * applyTransformation - generates the json request to apply a transformation
+   *
+   * @param {object} point point to apply transformation to
+   * @param {object} transformation  transformation to apply to the point
+   * @param {number} id an identifier for the generated request
+   * @return {string} the json request representation
+   */
+  applyTransformation(point, transformation, id) {
+    // check input points
+    if (
+      point == null ||
+      point.x == null ||
+      point.y == null ||
+      point.z == null ||
+      transformation == null ||
+      transformation[0] == null ||
+      transformation[1] == null ||
+      transformation[2] == null ||
+      transformation[3] == null ||
+      transformation[4] == null ||
+      transformation[5] == null ||
+      transformation[6] == null ||
+      transformation.length !== 7
+    ) {
+      return null;
+    }
+
+    // set up observations
+    const observations = {
+      // add point
+      point: {
+        x: point.x,
+        y: point.y,
+        z: point.z
+      },
+      // add transformation
+      transformation: {
+        tx: transformation[0],
+        ty: transformation[1],
+        tz: transformation[2],
+        q0: transformation[3],
+        q1: transformation[4],
+        q2: transformation[5],
+        q3: transformation[6],
+        m: 1.0
+      }
+    };
+
+    // build up applyTransformation request object
+    const message = JSON.stringify(
+      {
+        jsonrpc: "2.0",
+        id,
+        method: "applyTransformation",
+        params: observations
+      },
+      undefined,
+      4
+    );
+    return message;
+  },
+
+  /**
+   * cardanToQuat - generates the json request to transform cardan rotation to quaternions
+   *
+   * @param {object} coords to be transformed
+   * @param {number} id an identifier for the generated request
+   * @return {string} the json request representation
+   */
+  cardanToQuat(coords, id) {
+    // check input points
+    if (
+      coords == null ||
+      coords.Rx == null ||
+      coords.Ry == null ||
+      coords.Rz == null
+    ) {
+      return null;
+    }
+
+    // set up observations
+    const observations = {
+      rx: coords.Rx,
+      ry: coords.Ry,
+      rz: coords.Rz
+    };
+
+    // build up cardanRotation2Quaternion request object
+    const message = JSON.stringify(
+      {
+        jsonrpc: "2.0",
+        id,
+        method: "cardanRotation2Quaternion",
+        params: observations
+      },
+      undefined,
+      4
+    );
+    return message;
+  },
+
+  /**
    * fitCircle3DTscheby - generates the json request to fit a 3D-circle
    * with Tschebyscheff approximation
    *
@@ -35,6 +137,50 @@ module.exports = {
         method: "fitCircle3DTscheby",
         params: {
           observations
+        }
+      },
+      undefined,
+      4
+    );
+    return message;
+  },
+
+  /**
+   * fitCylinder - generates the json request to fit cylinder
+   *
+   * @param  {Array} points the points used to fit the plane
+   * @param  {number} id an identifier for the generated request
+   * @return {string} the json request representation
+   */
+  fitCylinder(points, id) {
+    // check input points
+    if (points == null) {
+      return null;
+    }
+    // set up observations
+    const observations = [];
+    let i;
+    for (i = 0; i < points.length; i++) {
+      // check point
+      if (points[i].x == null || points[i].y == null || points[i].z == null) {
+        return null;
+      }
+      // add point
+      observations.push({
+        x: points[i].x,
+        y: points[i].y,
+        z: points[i].z
+      });
+    }
+    // build up fitPlaneL2 request object
+    const message = JSON.stringify(
+      {
+        jsonrpc: "2.0",
+        id,
+        method: "fitCylinderL2",
+        params: {
+          observations,
+          approxType: "approxFromFirstTwoPoints"
         }
       },
       undefined,
@@ -80,6 +226,145 @@ module.exports = {
         params: {
           observations
         }
+      },
+      undefined,
+      4
+    );
+    return message;
+  },
+
+  /**
+   * fitPlaneRansac - generates the json request to fit a plane
+   * with RANSAC
+   *
+   * @param  {Array} points the points used to fit the plane
+   * @param  {number} tolerance the tolerance used to fit the plane
+   * @param  {number} id an identifier for the generated request
+   * @return {string} the json request representation
+   */
+  fitPlaneRansac(points, tolerance, id) {
+    // check input points
+    if (points == null || tolerance == null) {
+      return null;
+    }
+    // set up observations
+    const observations = [];
+    let i;
+    for (i = 0; i < points.length; i++) {
+      // check point
+      if (points[i].x == null || points[i].y == null || points[i].z == null) {
+        return null;
+      }
+      // add point
+      observations.push({
+        x: points[i].x,
+        y: points[i].y,
+        z: points[i].z
+      });
+    }
+    // build up fitPlaneL2 request object
+    const message = JSON.stringify(
+      {
+        jsonrpc: "2.0",
+        id,
+        method: "fitPlaneRansac",
+        params: {
+          observations,
+          tolerance
+        }
+      },
+      undefined,
+      4
+    );
+    return message;
+  },
+
+  /**
+   * invertTransformationParameters - generates the json request to invert transformation parameters
+   *
+   * @param {object} transformation transformation parameters to be inverted
+   * @param {number} id an identifier for the generated request
+   * @return {string} the json request representation
+   */
+  invertTransformationParameters(transformation, id) {
+    // check input points
+    if (
+      transformation == null ||
+      transformation.tx == null ||
+      transformation.ty == null ||
+      transformation.tz == null ||
+      transformation.q0 == null ||
+      transformation.q1 == null ||
+      transformation.q2 == null ||
+      transformation.q3 == null ||
+      transformation.m == null
+    ) {
+      return null;
+    }
+
+    // set up observations
+    const observations = {
+      // add transformation
+      transformation: {
+        tx: transformation.tx,
+        ty: transformation.ty,
+        tz: transformation.tz,
+        q0: transformation.q0,
+        q1: transformation.q1,
+        q2: transformation.q2,
+        q3: transformation.q3,
+        m: transformation.m
+      }
+    };
+
+    // build up invertTransformationParameters request object
+    const message = JSON.stringify(
+      {
+        jsonrpc: "2.0",
+        id,
+        method: "invertTransformationParameters",
+        params: observations
+      },
+      undefined,
+      4
+    );
+    return message;
+  },
+
+  /**
+   * quatToCardan - generates the json request to transform quaternions to cardan rotation
+   *
+   * @param {object} coord to be transformed
+   * @param {number} id an identifier for the generated request
+   * @return {string} the json request representation
+   */
+  quatToCardan(coords, id) {
+    // check input points
+    if (
+      coords == null ||
+      coords.q0 == null ||
+      coord.q1 == null ||
+      coords.q2 == null ||
+      coords.q3 == null
+    ) {
+      return null;
+    }
+
+    // set up observations
+    const observations = {
+      q0: coords.q0,
+      q1: coords.q1,
+      q2: coords.q2,
+      q3: coords.q3
+    };
+
+    // build up quaternion2CardanRotation request object
+    const message = JSON.stringify(
+      {
+        jsonrpc: "2.0",
+        id,
+        method: "quaternion2CardanRotation",
+        params: observations
       },
       undefined,
       4
@@ -216,291 +501,6 @@ module.exports = {
         id,
         method: "transformation3D6W",
         params: observations
-      },
-      undefined,
-      4
-    );
-    return message;
-  },
-
-  /**
-   * invertTransformationParameters - generates the json request to invert transformation parameters
-   *
-   * @param {object} transformation transformation parameters to be inverted
-   * @param {number} id an identifier for the generated request
-   * @return {string} the json request representation
-   */
-  invertTransformationParameters(transformation, id) {
-    // check input points
-    if (
-      transformation == null ||
-      transformation.tx == null ||
-      transformation.ty == null ||
-      transformation.tz == null ||
-      transformation.q0 == null ||
-      transformation.q1 == null ||
-      transformation.q2 == null ||
-      transformation.q3 == null ||
-      transformation.m == null
-    ) {
-      return null;
-    }
-
-    // set up observations
-    const observations = {
-      // add transformation
-      transformation: {
-        tx: transformation.tx,
-        ty: transformation.ty,
-        tz: transformation.tz,
-        q0: transformation.q0,
-        q1: transformation.q1,
-        q2: transformation.q2,
-        q3: transformation.q3,
-        m: transformation.m
-      }
-    };
-
-    // build up invertTransformationParameters request object
-    const message = JSON.stringify(
-      {
-        jsonrpc: "2.0",
-        id,
-        method: "invertTransformationParameters",
-        params: observations
-      },
-      undefined,
-      4
-    );
-    return message;
-  },
-
-  /**
-   * applyTransformation - generates the json request to apply a transformation
-   *
-   * @param {object} point point to apply transformation to
-   * @param {object} transformation  transformation to apply to the point
-   * @param {number} id an identifier for the generated request
-   * @return {string} the json request representation
-   */
-  applyTransformation(point, transformation, id) {
-    // check input points
-    if (
-      point == null ||
-      point.x == null ||
-      point.y == null ||
-      point.z == null ||
-      transformation == null ||
-      transformation[0] == null ||
-      transformation[1] == null ||
-      transformation[2] == null ||
-      transformation[3] == null ||
-      transformation[4] == null ||
-      transformation[5] == null ||
-      transformation[6] == null ||
-      transformation.length !== 7
-    ) {
-      return null;
-    }
-
-    // set up observations
-    const observations = {
-      // add point
-      point: {
-        x: point.x,
-        y: point.y,
-        z: point.z
-      },
-      // add transformation
-      transformation: {
-        tx: transformation[0],
-        ty: transformation[1],
-        tz: transformation[2],
-        q0: transformation[3],
-        q1: transformation[4],
-        q2: transformation[5],
-        q3: transformation[6],
-        m: 1.0
-      }
-    };
-
-    // build up applyTransformation request object
-    const message = JSON.stringify(
-      {
-        jsonrpc: "2.0",
-        id,
-        method: "applyTransformation",
-        params: observations
-      },
-      undefined,
-      4
-    );
-    return message;
-  },
-
-  /**
-   * quatToCardan - generates the json request to transform quaternions to cardan rotation
-   *
-   * @param {object} coord to be transformed
-   * @param {number} id an identifier for the generated request
-   * @return {string} the json request representation
-   */
-  quatToCardan(coords, id) {
-    // check input points
-    if (
-      coords == null ||
-      coords.q0 == null ||
-      coord.q1 == null ||
-      coords.q2 == null ||
-      coords.q3 == null
-    ) {
-      return null;
-    }
-
-    // set up observations
-    const observations = {
-      q0: coords.q0,
-      q1: coords.q1,
-      q2: coords.q2,
-      q3: coords.q3
-    };
-
-    // build up quaternion2CardanRotation request object
-    const message = JSON.stringify(
-      {
-        jsonrpc: "2.0",
-        id,
-        method: "quaternion2CardanRotation",
-        params: observations
-      },
-      undefined,
-      4
-    );
-    return message;
-  },
-
-  /**
-   * cardanToQuat - generates the json request to transform cardan rotation to quaternions
-   *
-   * @param {object} coord to be transformed
-   * @param {number} id an identifier for the generated request
-   * @return {string} the json request representation
-   */
-  cardanToQuat(coords, id) {
-    // check input points
-    if (
-      coords == null ||
-      coords.Rx == null ||
-      coords.Ry == null ||
-      coords.Rz == null
-    ) {
-      return null;
-    }
-
-    // set up observations
-    const observations = {
-      rx: coords.Rx,
-      ry: coords.Ry,
-      rz: coords.Rz
-    };
-
-    // build up cardanRotation2Quaternion request object
-    const message = JSON.stringify(
-      {
-        jsonrpc: "2.0",
-        id,
-        method: "cardanRotation2Quaternion",
-        params: observations
-      },
-      undefined,
-      4
-    );
-    return message;
-  },
-
-  /**
-   * fitPlaneRansac - generates the json request to fit a plane
-   * with RANSAC
-   *
-   * @param  {Array} points the points used to fit the plane
-   * @param  {number} tolerance the tolerance used to fit the plane
-   * @param  {number} id an identifier for the generated request
-   * @return {string} the json request representation
-   */
-  fitPlaneRansac(points, tolerance, id) {
-    // check input points
-    if (points == null || tolerance == null) {
-      return null;
-    }
-    // set up observations
-    const observations = [];
-    let i;
-    for (i = 0; i < points.length; i++) {
-      // check point
-      if (points[i].x == null || points[i].y == null || points[i].z == null) {
-        return null;
-      }
-      // add point
-      observations.push({
-        x: points[i].x,
-        y: points[i].y,
-        z: points[i].z
-      });
-    }
-    // build up fitPlaneL2 request object
-    const message = JSON.stringify(
-      {
-        jsonrpc: "2.0",
-        id,
-        method: "fitPlaneRansac",
-        params: {
-          observations,
-          tolerance
-        }
-      },
-      undefined,
-      4
-    );
-    return message;
-  },
-
-  /**
-   * fitCylinder - generates the json request to fit cylinder
-   *
-   * @param  {Array} points the points used to fit the plane
-   * @param  {number} id an identifier for the generated request
-   * @return {string} the json request representation
-   */
-  fitCylinder(points, id) {
-    // check input points
-    if (points == null) {
-      return null;
-    }
-    // set up observations
-    const observations = [];
-    let i;
-    for (i = 0; i < points.length; i++) {
-      // check point
-      if (points[i].x == null || points[i].y == null || points[i].z == null) {
-        return null;
-      }
-      // add point
-      observations.push({
-        x: points[i].x,
-        y: points[i].y,
-        z: points[i].z
-      });
-    }
-    // build up fitPlaneL2 request object
-    const message = JSON.stringify(
-      {
-        jsonrpc: "2.0",
-        id,
-        method: "fitCylinderL2",
-        params: {
-          observations,
-          approxType: "approxFromFirstTwoPoints"
-        }
       },
       undefined,
       4
